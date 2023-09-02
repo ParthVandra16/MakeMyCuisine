f72914616d242ef92c46e6569ec67797271506f5
ParthVandra16 <106701842+ParthVandra16@users.noreply.github.com>
2023-09-02 12:44:34 -0400

    Done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 
 
-function Footer(){
-return(
-    <footer className="dark-footer skin-dark-footer">
+function Footer() {
+    return (
+        <footer className="dark-footer skin-dark-footer">
 
-    
-    <div className="footer-bottom">
-        <div className="container">
-            <div className="row align-items-center">
-                
-                <div className="col-lg-6 col-md-6">
-                    <p className="mb-0">© 2021 Make My Recipe Designd By Harsh | Rahul | Happy  All Rights Reserved</p>
+
+            <div className="footer-bottom">
+                <div className="container">
+                    <div className="row align-items-center">
+
+                        <div className="col-lg-6 col-md-6">
+                            <p className="mb-0">© 2021 Make My Recipe Designd By Harsh | Rahul | Parth  All Rights Reserved</p>
+                        </div>
+
+                        <div className="col-lg-6 col-md-6 text-right">
+                            <ul className="footer-bottom-social">
+                                <li><a href="freelancing.html#"><i className="ti-facebook"></i></a></li>
+                                <li><a href="freelancing.html#"><i className="ti-twitter"></i></a></li>
+                                <li><a href="freelancing.html#"><i className="ti-instagram"></i></a></li>
+                                <li><a href="freelancing.html#"><i className="ti-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+
+                    </div>
                 </div>
-                
-                <div className="col-lg-6 col-md-6 text-right">
-                    <ul className="footer-bottom-social">
-                        <li><a href="freelancing.html#"><i className="ti-facebook"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-twitter"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-instagram"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                
             </div>
-        </div>
-    </div>
-</footer>
+        </footer>
 
 
 
 
 
-);
+    );
 
 }
 
